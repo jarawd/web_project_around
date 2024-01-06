af48a5bb9e1ef1c394902d672c1b34bd15e443bb
jara <devjara11@gmail.com>
2024-01-05 21:24:37 -0600

    feat: develop website

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Tripleten web_project_around
+# Nombre del proyecto:
+
+Tripleten web_project_around
+
+# Descripción del proyecto:
+
+Sitio web que contiene fotografias de lugares de Estados Unidos. Además tiene la funcionalidad (por ahora simulada) de agregar un nuevo perfil de la persona quien visita esos lugares. Así como también la posibilidad de dar "Me gusta" a alguna fotografía de nuestro interés.
+
+# Tecnologías usadas:
+
+- HTML.
+- CSS Grid.
+- Flexbox.
+- Metodología BEM.
+- Media Queries para el diseño responsivo.
+- Javascript (Manejo del DOM).
+- Git.
+
+# Enlace al sitio web:
---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+let content = document.querySelector(".page");
+let popupOpen = content.querySelector(".popup");
+let addProfile = content.querySelector(".profile__edit");
+let popupClose = content.querySelector(".popup__close-icon");
+let nameContent = content.querySelector(".profile__title");
+let hobbyContent = content.querySelector(".profile__hobby");
+let inputName = content.querySelector(".popup__form-item_name");
+let inputHobby = content.querySelector(".popup__form-item_hobby");
+let saveInfo = content.querySelector(".popup__form-button");
+
+function showPopup() {
+  inputName.value = nameContent.textContent;
+  inputHobby.value = hobbyContent.textContent;
+  popupOpen.classList.add("popup__opened");
+}
+
+function closePopup() {
+  popupOpen.classList.remove("popup__opened");
+}
+
+function editInfo(e) {
+  e.preventDefault();
+  nameContent.textContent = inputName.value;
+  hobbyContent.textContent = inputHobby.value;
+  closePopup();
+}
+
+addProfile.addEventListener("click", showPopup);
+popupClose.addEventListener("click", closePopup);
+saveInfo.addEventListener("click", editInfo);
