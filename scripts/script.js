5f4a48b0969f93a9f313e6df7f0b5c4619c3f5ed
jara <devjara11@gmail.com>
2024-01-23 17:57:56 -0600

    feat/cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,107 @@
-const content = document.querySelector(".page");
-const popupOpen = content.querySelector(".popup");
-const addProfile = content.querySelector(".profile__edit");
-const popupClose = content.querySelector(".popup__close");
-const nameContent = content.querySelector(".profile__title");
-const hobbyContent = content.querySelector(".profile__hobby");
-const inputName = content.querySelector(".popup__form-item_name");
-const inputHobby = content.querySelector(".popup__form-item_hobby");
-const saveInfo = content.querySelector(".popup__form-button");
+const container = document.querySelector(".page");
+const addProfile = container.querySelector(".profile__edit");
+const nameProfile = container.querySelector(".profile__title");
+const hobbyProfile = container.querySelector(".profile__hobby");
+const addImage = container.querySelector(".profile__add");
 
-function showPopup() {
-  inputName.value = nameContent.textContent;
-  inputHobby.value = hobbyContent.textContent;
-  popupOpen.classList.add("popup__opened");
+/* Popup template */
+const popupTemplate = document.querySelector("#popup-template").content;
+const popup = popupTemplate.querySelector(".popup").cloneNode(true);
+const inputName = popup.querySelector(".popup__form-item_name");
+const inputHobby = popup.querySelector(".popup__form-item_hobby");
+const popupSaveInfo = popup.querySelector(".popup__form-button");
+const popupClose = popup.querySelector(".popup__close");
+
+function showPopup(title, input1, input2, buttonName) {
+  popup.querySelector(".popup__title").textContent = title;
+  popup.querySelector(".popup__form-item_name").placeholder = input1;
+  popup.querySelector(".popup__form-item_hobby").placeholder = input2;
+  popup.querySelector(".popup__form-button").textContent = buttonName;
+  popup.classList.add("popup__opened");
+  container.prepend(popup);
 }
 
 function closePopup() {
-  popupOpen.classList.remove("popup__opened");
+  popup.classList.remove("popup__opened");
 }
 
 function editInfo(e) {
   e.preventDefault();
-  nameContent.textContent = inputName.value;
-  hobbyContent.textContent = inputHobby.value;
   closePopup();
 }
 
-addProfile.addEventListener("click", showPopup);
-popupClose.addEventListener("click", closePopup);
-saveInfo.addEventListener("click", editInfo);
+function addCard(e) {
+  e.preventDefault();
+}
+
+container.addEventListener("click", (e) => {
+  if (e.target === popupClose) {
+    closePopup();
+  }
+  if (e.target === addProfile) {
+    inputName.value = nameProfile.textContent;
+    inputHobby.value = hobbyProfile.textContent;
+    showPopup("Edit profile", "Name", "Hobby", "Save");
+  }
+
+  if (e.target === popupSaveInfo) {
+    e.preventDefault();
+    nameProfile.textContent = inputName.value;
+    hobbyProfile.textContent = inputHobby.value;
+    closePopup();
+  }
+
+  if (e.target === addImage) {
+    inputName.value = "";
+    inputHobby.value = "";
+    showPopup("Nuevo lugar", "Título", "Enlace de la imagen", "Crear");
+    if (e.target === popupSaveInfo) {
+      e.preventDefault();
+    }
+  }
+});
+
+/* Sprint 6 Code */
+const cards = document.querySelector(".elements");
+const cardContainer = document.querySelector("#card-template").content;
+const page = document.querySelector(".page");
+const addCardButton = container.querySelector(".profile__add");
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+initialCards.forEach((el) => {
+  const card = cardContainer.querySelector(".elements__card").cloneNode(true);
+  card.querySelector(".elements__img").src = el.link;
+  card.querySelector(".elements__img").alt = el.name;
+  card.querySelector(".elements__title").textContent = el.name;
+  cards.append(card);
+});
+
+addCardButton.addEventListener("click", () => {
+  showPopup("Nuevo lugar", "Título", "Enlace a la Imagen", "Crear");
+});
