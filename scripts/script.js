3cc4f20cf02107a82ab21316f9ec02686b2d56a6
jara <devjara11@gmail.com>
2024-01-24 20:38:36 -0600

    feat/add images

--- scripts/script.js
+++ scripts/script.js
@@ -1,71 +1,101 @@
+/* Sprint 5 Variables */
+
 const container = document.querySelector(".page");
 const addProfile = container.querySelector(".profile__edit");
 const nameProfile = container.querySelector(".profile__title");
 const hobbyProfile = container.querySelector(".profile__hobby");
 const addImage = container.querySelector(".profile__add");
 
-/* Popup template */
+/* Sprint 6 Variables */
+
+/* Popup Profile template */
 const popupTemplate = document.querySelector("#popup-template").content;
-const popup = popupTemplate.querySelector(".popup").cloneNode(true);
-const inputName = popup.querySelector(".popup__form-item_name");
-const inputHobby = popup.querySelector(".popup__form-item_hobby");
-const popupSaveInfo = popup.querySelector(".popup__form-button");
-const popupClose = popup.querySelector(".popup__close");
+const popupProfile = popupTemplate.querySelector(".popup").cloneNode(true);
+const inputNameProfile = popupProfile.querySelector(".popup__form-item_name");
+const inputHobbyProfile = popupProfile.querySelector(".popup__form-item_info");
+const saveProfile = popupProfile.querySelector(".popup__form-button");
+const popupProfileClose = popupProfile.querySelector(".popup__close");
 
-function showPopup(title, input1, input2, buttonName) {
-  popup.querySelector(".popup__title").textContent = title;
-  popup.querySelector(".popup__form-item_name").placeholder = input1;
-  popup.querySelector(".popup__form-item_hobby").placeholder = input2;
-  popup.querySelector(".popup__form-button").textContent = buttonName;
-  popup.classList.add("popup__opened");
-  container.prepend(popup);
-}
+/* Popup Add Image template */
+const popupImage = popupTemplate.querySelector(".popup").cloneNode(true);
+const inputNameImage = popupImage.querySelector(".popup__form-item_name");
+const inputHobbyImage = popupImage.querySelector(".popup__form-item_info");
+const saveImage = popupImage.querySelector(".popup__form-button");
+const popupImageClose = popupImage.querySelector(".popup__close");
 
-function closePopup() {
-  popup.classList.remove("popup__opened");
-}
-
-function editInfo(e) {
-  e.preventDefault();
-  closePopup();
-}
-
-function addCard(e) {
-  e.preventDefault();
-}
-
-container.addEventListener("click", (e) => {
-  if (e.target === popupClose) {
-    closePopup();
-  }
-  if (e.target === addProfile) {
-    inputName.value = nameProfile.textContent;
-    inputHobby.value = hobbyProfile.textContent;
-    showPopup("Edit profile", "Name", "Hobby", "Save");
-  }
-
-  if (e.target === popupSaveInfo) {
-    e.preventDefault();
-    nameProfile.textContent = inputName.value;
-    hobbyProfile.textContent = inputHobby.value;
-    closePopup();
-  }
-
-  if (e.target === addImage) {
-    inputName.value = "";
-    inputHobby.value = "";
-    showPopup("Nuevo lugar", "Título", "Enlace de la imagen", "Crear");
-    if (e.target === popupSaveInfo) {
-      e.preventDefault();
-    }
-  }
-});
-
-/* Sprint 6 Code */
 const cards = document.querySelector(".elements");
 const cardContainer = document.querySelector("#card-template").content;
 const page = document.querySelector(".page");
 const addCardButton = container.querySelector(".profile__add");
+
+function showPopupProfile(title, input1, input2, buttonName) {
+  popupProfile.querySelector(".popup__title").textContent = title;
+  popupProfile.querySelector(".popup__form-item_name").placeholder = input1;
+  popupProfile.querySelector(".popup__form-item_info").placeholder = input2;
+  popupProfile.querySelector(".popup__form-button").textContent = buttonName;
+  popupProfile.classList.add("popup__opened");
+  container.prepend(popupProfile);
+}
+
+function showPopupImage(title, input1, input2, buttonName) {
+  popupImage.querySelector(".popup__title").textContent = title;
+  popupImage.querySelector(".popup__form-item_name").placeholder = input1;
+  popupImage.querySelector(".popup__form-item_info").placeholder = input2;
+  popupImage.querySelector(".popup__form-button").textContent = buttonName;
+  popupImage.classList.add("popup__opened");
+  container.prepend(popupImage);
+}
+
+function closePopup(e) {
+  if (e.target === popupProfileClose || e.target === saveProfile) {
+    popupProfile.classList.remove("popup__opened");
+  }
+
+  if (e.target === popupImageClose || e.target === saveImage) {
+    popupImage.classList.remove("popup__opened");
+    popupImage.querySelector(".popup__form-item_name").value = "";
+    popupImage.querySelector(".popup__form-item_info").value = "";
+  }
+}
+
+container.addEventListener("click", (e) => {
+  if (e.target === popupProfileClose || e.target === popupImageClose) {
+    closePopup(e);
+  }
+
+  if (e.target === addProfile) {
+    inputNameProfile.value = nameProfile.textContent;
+    inputHobbyProfile.value = hobbyProfile.textContent;
+    showPopupProfile("Edit profile", "Name", "Hobby", "Save");
+  }
+
+  if (e.target === saveProfile) {
+    e.preventDefault();
+    if (inputNameProfile.value && inputHobbyProfile.value) {
+      nameProfile.textContent = inputNameProfile.value;
+      hobbyProfile.textContent = inputHobbyProfile.value;
+      closePopup(e);
+    }
+  }
+
+  if (e.target === addImage) {
+    showPopupImage("Nuevo lugar", "Título", "Enlace de la imagen", "Crear");
+  }
+
+  if (e.target === saveImage) {
+    e.preventDefault();
+    let imageName = popupImage.querySelector(".popup__form-item_name").value;
+    let pathImage = popupImage.querySelector(".popup__form-item_info").value;
+    const card = cardContainer.querySelector(".elements__card").cloneNode(true);
+    card.querySelector(".elements__img").src = pathImage;
+    card.querySelector(".elements__img").alt = imageName;
+    card.querySelector(".elements__title").textContent = imageName;
+    cards.prepend(card);
+    if (inputNameImage.value && inputHobbyImage.value) {
+      closePopup(e);
+    }
+  }
+});
 
 const initialCards = [
   {
@@ -100,8 +130,4 @@ initialCards.forEach((el) => {
   card.querySelector(".elements__img").alt = el.name;
   card.querySelector(".elements__title").textContent = el.name;
   cards.append(card);
-});
-
-addCardButton.addEventListener("click", () => {
-  showPopup("Nuevo lugar", "Título", "Enlace a la Imagen", "Crear");
 });
